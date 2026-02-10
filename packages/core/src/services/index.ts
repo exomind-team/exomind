667ad8bb063ab1b7c1e84942ed26c64ea35cc40c
ARCJ137442 <61109168+ARCJ137442@users.noreply.github.com>
2026-02-10 19:01:30 +0800

    feat: 迁移 message-storage.ts 到 Monorepo core 包 [message-storage.ts]

--- packages/core/src/services/index.ts
+++ packages/core/src/services/index.ts
@@ -3,4 +3,6 @@
  */
 
 export * from './timeblock.service';
+export * from './message-storage';
 export type { TimeBlockService } from './timeblock.service';
+export type { MessageStorage, ChatMessage, SyncMessage } from './message-storage';
