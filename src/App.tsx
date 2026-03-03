9c9a83ee0c7512bb6acde43350a7ff63483ce57b
Argon@Termux <Argon@github.com>
2026-03-04 01:58:11 +0800

    refactor(sync): centralize timeblock sync lifecycle coordinator

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { appRouter } from "@/routes";
 import { ThemeController } from "@/components/ThemeController";
 import { Toaster } from "@/components/ui/toaster";
 import { UpdateToast } from "@/ui/components/UpdateToast";
+import { TimeBlockSyncCoordinator } from "@/ui/app/components/TimeBlockSyncCoordinator";
 import {
   initUpdateChecker,
   destroyUpdateChecker,
@@ -19,6 +20,7 @@ function App() {
   return (
     <>
       <ThemeController />
+      <TimeBlockSyncCoordinator />
       <RouterProvider router={appRouter} />
       <Toaster />
       <UpdateToast />
